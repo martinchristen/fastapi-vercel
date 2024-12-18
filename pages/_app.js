204a82010d8e6b3cd924d1d9b35c974a2f1b9c57
Martin Christen <martin.christen@fhnw.ch>
2024-12-18 11:02:11 +0100

    first version of app

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+
+
+
+
+export default function App() {
+    return (
+      <>
+        <h1>Hallo App</h1>
+      </>
+    );
+  }
---
+++ pages/info.js
@@ -0,0 +1,11 @@
+
+
+
+
+export default function Info() {
+    return (
+      <>
+        <h1>Hallo Info</h1>
+      </>
+    );
+  }
